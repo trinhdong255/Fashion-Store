e2705611f7d3c39691e4ba04a05bf2f57275eee2
Trịnh Đồng <trinhdong255@gmail.com>
2025-05-14 11:22:45 +0700

    feat: add SwiperProducts for page Home

--- src/components/SwiperProducts/index.jsx
+++ src/components/SwiperProducts/index.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardActionArea,
@@ -7,84 +8,108 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import { useEffect } from "react";
-import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import axios from "axios";
 
 import styles from "./index.module.css";
 
 import "swiper/css";
 import "swiper/css/navigation";
-import { useListProductsQuery } from "@/services/api/product";
 
-// const navCategories = [
-//   {
-//     title: "SẢN PHẨM BÁN CHẠY",
-//     description: [
-//       "ÁO THUN",
-//       "ÁO SƠ MI",
-//       "ÁO KHOÁC",
-//       "QUẦN DÀI",
-//       "QUẦN SHORTS",
-//       "PHỤ KIỆN",
-//     ],
-//     showButton: true,
-//   },
-//   {
-//     title: "SẢN PHẨM MỚI NHẤT",
-//     description: [
-//       "ÁO THUN",
-//       "ÁO SƠ MI",
-//       "ÁO KHOÁC",
-//       "QUẦN DÀI",
-//       "QUẦN SHORTS",
-//       "PHỤ KIỆN",
-//     ],
-//     showButton: true,
-//   },
-// ];
+const navCategories = [
+  {
+    title: "SẢN PHẨM BÁN CHẠY",
+    showButton: true,
+  },
+  {
+    title: "SẢN PHẨM MỚI NHẤT",
+    showButton: true,
+  },
+];
 
 const SwiperProducts = () => {
   const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const { data: dataProducts } = useListProductsQuery();
+  // Fetch categories and products
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    const fetchCategories = axios.get(
+      "http://222.255.119.40:8080/adamstore/v1/categories?pageNo=1&pageSize=10",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const fetchProducts = axios.get(
+      "http://222.255.119.40:8080/adamstore/v1/products",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    Promise.all([fetchCategories, fetchProducts])
+      .then(([categoriesResponse, productsResponse]) => {
+        setCategories(categoriesResponse.data.result.items);
+        setProducts(productsResponse.data.result.items);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error("Lỗi khi fetch dữ liệu:", err);
+        setError(err.message || "Không thể tải dữ liệu");
+        setIsLoading(false);
+      });
+  }, []);
 
   const handleClick = () => {
-    navigate("/listProducts");
+    navigate("/product-lists");
   };
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    // Cleanup function
-    return () => {
-      controller.abort();
-    };
-  }, []);
 
   return (
     <>
-      {/* {navCategories.map((category, index) => (
+      {navCategories.map((category, index) => (
         <div key={index}>
           <Stack className={styles.topSellingProducts}>
             <h3>{category.title}</h3>
             <nav className={styles.navigationTopSellingProducts}>
-              <ul>
-                {category.description.map((item, index) => (
-                  <li key={index}>
-                    <Link to="#">{item}</Link>
-                  </li>
-                ))}
-              </ul>
+              {isLoading ? (
+                <p>Đang tải danh mục...</p>
+              ) : error ? (
+                <p>Lỗi khi tải danh mục: {error}</p>
+              ) : (
+                <ul>
+                  {categories
+                    .filter((item) => item.status === "ACTIVE")
+                    .map((item) => (
+                      <li key={item.id}>
+                        <Link to={`/product-lists?category=${item.id}`}>
+                          {item.name}
+                        </Link>
+                      </li>
+                    ))}
+                </ul>
+              )}
             </nav>
           </Stack>
 
           <Swiper
             loop={true}
-            slidesPerView={3}
-            slidesPerGroup={1}
-            spaceBetween={30}
+            slidesPerView={4}
+            slidesPerGroup={2}
+            spaceBetween={20}
             centeredSlides={true}
             autoplay={{
               delay: 3000,
@@ -93,61 +118,117 @@ const SwiperProducts = () => {
             navigation={true}
             modules={[Autoplay, Pagination, Navigation]}
             style={{
-              margin: "50px 0",
-            }}>
-            {(dataProducts?.products || []).map((product) => (
-              <SwiperSlide key={product.id}>
-                <Card onClick={() => navigate(`/productDetails/${product.id}`)}>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="300"
-                      image={product.thumbnail}
-                      alt={product.title}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h6" component="div">
-                        {product.title}
-                      </Typography>
-                      <Typography gutterBottom variant="body2" component="div">
-                        Rating: {product.rating}
-                      </Typography>
-                      <Typography
-                        gutterBottom
-                        variant="body1"
-                        component="div"
-                        sx={{
-                          display: "flex",
-                          flexDirection: "row",
-                          alignItems: "center",
-                        }}>
-                        <Typography
-                          variant="body2"
-                          sx={{
-                            color: "text.secondary",
-                            textDecoration: "line-through",
-                            fontSize: "1rem",
-                          }}>
-                          ${product.price}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          sx={{
-                            color: "text.primary",
-                            fontSize: "1.2rem",
-                            marginLeft: "10px",
-                          }}>
-                          ${product.price}
-                        </Typography>
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
+              "--swiper-navigation-color": "var(--text-color)",
+              minHeight: 500,
+              marginBottom: "20px",
+            }}
+          >
+            {isLoading ? (
+              <SwiperSlide>
+                <Typography>Đang tải sản phẩm...</Typography>
               </SwiperSlide>
-            ))}
+            ) : error ? (
+              <SwiperSlide>
+                <Typography>Lỗi khi tải sản phẩm: {error}</Typography>
+              </SwiperSlide>
+            ) : (
+              products.map((product) => (
+                <SwiperSlide key={product.id}>
+                  <Card
+                    onClick={() => navigate(`/product-detail/${product.id}`)}
+                  >
+                    <CardActionArea sx={{ minHeight: "100%" }}>
+                      <CardMedia
+                        component="img"
+                        height="300"
+                        width="100%"
+                        image={product.images?.[0]?.imageUrl}
+                        alt={product.name}
+                        loading="lazy"
+                        sx={{
+                          objectFit: "cover",
+                          objectPosition: "center",
+                          
+                        }}
+                      />
+                      <CardContent sx={{ minHeight: "100%" }}>
+                        <Typography
+                          sx={{
+                            display: "-webkit-box",
+                            WebkitBoxOrient: "vertical",
+                            WebkitLineClamp: 2,
+                            overflow: "hidden",
+                          }}
+                          gutterBottom
+                          variant="h6"
+                          component="div"
+                        >
+                          {product.name}
+                        </Typography>
+                        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                          <Typography
+                          gutterBottom
+                          variant="body2"
+                          component="div"
+                        >
+                          Đánh giá: {product.averageRating || 0}
+                        </Typography>
+                        <Typography
+                          gutterBottom
+                          variant="body2"
+                          component="div"
+                        >
+                          Đã bán: {product.soldQuantity || 0}
+                        </Typography>
+                        </Box>
+                        
+                        <Typography
+                          gutterBottom
+                          variant="body1"
+                          component="div"
+                          sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                          }}
+                        >
+                          <Typography
+                            variant="body2"
+                            sx={{
+                              color: "text.primary",
+                              fontSize: "1.2rem",
+                            }}
+                          >
+                            {product.price?.toLocaleString("vi-VN")} VNĐ
+                          </Typography>
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </SwiperSlide>
+              ))
+            )}
           </Swiper>
+
+          {category.showButton && (
+            <Stack alignItems={"center"}>
+              <Button
+                variant="contained"
+                size="large"
+                sx={{
+                  backgroundColor: "var(--footer-background-color)",
+                  fontSize: "1rem",
+                  marginBottom: "100px",
+                  padding: "12px 24px",
+                }}
+                onClick={handleClick}
+              >
+                XEM THÊM
+              </Button>
+            </Stack>
+          )}
         </div>
-      ))} */}
+      ))}
     </>
   );
 };
